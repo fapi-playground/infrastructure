3c7d700470a72b1e49f8fb12b06def2512aebc24
greg <grzegorz.sowa314@gmail.com>
2025-03-26 16:49:06 +0100

    new synapse config

--- create_synapse_client.sh
+++ create_synapse_client.sh
@@ -1,11 +1,13 @@
-code_client=$(docker compose -f docker-compose.yml exec hydra \
+code_client=$(docker compose exec hydra \
     hydra create client \
     --endpoint http://127.0.0.1:4445 \
     --grant-type authorization_code,refresh_token \
     --response-type code,id_token \
     --format json \
     --scope openid --scope profile --scope email \
-    --redirect-uri http://127.0.0.1:5555/callback);
+    --redirect-uri https://matrix.gregs-homelab.com/_synapse/client/oidc/callback \
+    --token-endpoint-auth-method none
+    --skip-consent);
 code_client_id=$(echo $code_client | jq -r '.client_id');
 code_client_secret=$(echo $code_client | jq -r '.client_secret');
 echo 'synapse client id: ';
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+#set -x
+
+# set up data & secrets dir with the right ownerships in the default location
+# to stop docker autocreating them with random owners.
+# originally these were checked into the git repo, but that's pretty ugly, so doing it here instead.
+mkdir -p data/{element-web,postgres,synapse}
+mkdir -p secrets/{postgres,synapse}
+
+# create blank secrets to avoid docker creating empty directories in the host
+touch secrets/postgres/postgres_password \
+      secrets/synapse/signing.key
+
+# grab an env if we don't have one already
+if [[ ! -e .env  ]]; then
+    cp .env-sample .env
+
+    sed -ri.orig "s/^USER_ID=/USER_ID=$(id -u)/" .env
+    sed -ri.orig "s/^GROUP_ID=/GROUP_ID=$(id -g)/" .env
+
+    read -p "Enter base domain name (e.g. example.com): " DOMAIN
+    sed -ri.orig "s/example.com/$DOMAIN/" .env
+
+else
+    echo ".env already exists; move it out of the way first to re-setup"
+fi
+
